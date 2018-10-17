82f894dd5d71b7867d7191dd2f59d13740a630b0
Dan Nepejchal <dan.nepejchal@gmail.com>
2018-10-17 21:06:50 +0200

    address clean

--- orders/forms.py
+++ orders/forms.py
@@ -12,10 +12,8 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = '__all__'
 
-    def clean(self):
-        form_data = self.cleaned_data
-        address = form_data['address'].as_dict()
-        print [len(address[field]) for field in ['street_number', 'route', 'country_code', 'postal_code', 'locality']]
+    def clean_address(self):
+        address = self.cleaned_data['address'].as_dict()
         if not all(len(address[field]) for field in ['street_number', 'route', 'country_code', 'postal_code', 'locality']):
             raise ValidationError('Please enter full address', 'invalid')
         return form_data
