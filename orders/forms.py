aa4ba042dc89fc418e5fb87b633c63865417ed58
Jakub Kalab <jakubkalab@gmail.com>
2019-02-28 21:23:12 +0100

    remove address form form

--- orders/forms.py
+++ orders/forms.py
@@ -2,18 +2,11 @@
 from __future__ import unicode_literals
 from django import forms
 from django.core.exceptions import ValidationError
-from address.forms import AddressField
 from .models import Order
 
 class OrderForm(forms.ModelForm):
-    address = AddressField()
 
     class Meta:
         model = Order
         exclude = ['status']
 
-    def clean_address(self):
-        address = self.cleaned_data['address'].as_dict()
-        if not all(len(address[field]) for field in ['street_number', 'route', 'country_code', 'postal_code', 'locality']):
-            raise ValidationError('Please enter full address', 'invalid')
-        return form_data
